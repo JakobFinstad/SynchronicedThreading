9659792b96636a6f74a584dfee441645f48b0e9f
Ertshus <siverer@stud.ntnu.no>
2023-02-21 11:01:06 +0100

    Added the main-class and the methods.

--- src/main/java/no/ntnu/IDATA2305/Group14/Main.java
+++ src/main/java/no/ntnu/IDATA2305/Group14/Main.java
@@ -1,7 +1,22 @@
 package no.ntnu.IDATA2305.Group14;
 
+import no.ntnu.IDATA2305.Group14.booking.MovieTicketServer;
+import no.ntnu.IDATA2305.Group14.booking.MovieTicketClient;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MovieTicketServer movieTicketServer = new MovieTicketServer("Troll", 10);
+
+        // Creating 4 threads
+        Thread t1 = new MovieTicketClient("Xiangming", 3, movieTicketServer);
+        Thread t2 = new MovieTicketClient("Ilaria", 2, movieTicketServer);
+        Thread t3 = new MovieTicketClient("Sam", 3, movieTicketServer);
+        Thread t4 = new MovieTicketClient("Andreas", 4, movieTicketServer);
+
+        // Starting all threads
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
 }

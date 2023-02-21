35dd850d5f9355cb9b11c9614a8edf38fd51fdee
Ertshus <siverer@stud.ntnu.no>
2023-02-21 10:46:13 +0100

    Almost finished with the MovieTicketServer.

---
+++ src/main/java/no/ntnu/IDATA2305/Group14/booking/MovieTicketServer.java
@@ -0,0 +1,25 @@
+package no.ntnu.IDATA2305.Group14.booking;
+
+public class MovieTicketServer {
+    private String movieName;
+    private int availableSeats;
+
+    public MovieTicketServer(String movieName, int availableSeats) {
+        this.movieName = movieName;
+        this.availableSeats = availableSeats;
+    }
+
+
+    public void bookTicket(String customerName, int numberOfSeats) {
+        System.out.println("Hi," + customerName + " : " + availableSeats + " : Seats available for " + movieName);
+
+        // TBD
+        if ((availableSeats - numberOfSeats) < 0) {
+            System.out.println("There are currently only " + availableSeats
+                    + ", which means you cannot book " + numberOfSeats + " seats.");
+        } else {
+            this.availableSeats = availableSeats - numberOfSeats;
+            System.out.println("Hi," + customerName + " : " + numberOfSeats + " Seats booked successfully for" + movieName);
+        }
+    }
+}

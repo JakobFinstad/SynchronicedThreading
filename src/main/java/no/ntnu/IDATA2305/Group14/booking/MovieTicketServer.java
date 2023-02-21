eceee13ef5308945299517619ebf45d6d1b81a39
Ertshus <siverer@stud.ntnu.no>
2023-02-21 12:11:30 +0100

    Added javadoc.

--- src/main/java/no/ntnu/IDATA2305/Group14/booking/MovieTicketServer.java
+++ src/main/java/no/ntnu/IDATA2305/Group14/booking/MovieTicketServer.java
@@ -1,15 +1,30 @@
 package no.ntnu.IDATA2305.Group14.booking;
 
+/**
+ * A class that represents a server where
+ */
 public class MovieTicketServer {
     private String movieName;
     private int availableSeats;
 
+    /**
+     * Constructor for the class.
+     *
+     * @param movieName name of the movie
+     * @param availableSeats available seats in the movie theatre
+     */
     public MovieTicketServer(String movieName, int availableSeats) {
         this.movieName = movieName;
         this.availableSeats = availableSeats;
     }
 
-
+    /**
+     * Prints out the amount of tickets available.
+     * If there not enough tickets, one will not be able to order.
+     * If there is enough one will get to order, and the amount is updated.
+     * @param customerName name of the customer
+     * @param numberOfSeats number of seats available in the theatre
+     */
     public void bookTicket(String customerName, int numberOfSeats) {
         System.out.println("Hi, " + customerName + ". " + availableSeats + " Seats available for " + movieName);
 

047881e79715c8f67e765684ada2bc805bfa21c0
JakobFinstad <jakobf44@gmail.com>
2023-02-21 10:45:46 +0100

    Implementing some of the features in MovieTicketClient.java

--- src/main/java/no/ntnu/IDATA2305/Group14/booking/MovieTicketClient.java
+++ src/main/java/no/ntnu/IDATA2305/Group14/booking/MovieTicketClient.java
@@ -1,11 +1,29 @@
 package no.ntnu.IDATA2305.Group14.booking;
 
-public class MovieTicketClient extends Thread{
-    public MovieTicketClient() {
+public class MovieTicketClient extends Thread {
 
+    private String customer;
+    private int numberOfTickets;
+    public MovieTicketClient(String customer, int NumberOfTickets) {
+        setCustomer(customer);
+        setNumberOfTickets(numberOfTickets);
     }
 
     public void run(){
 
+    }
+
+    private void setCustomer(String customer) {
+        if (customer.isEmpty() || customer == null) {
+            throw new IllegalArgumentException("Customer cannot be empty!");
+        }
+        this.customer = customer;
+    }
+
+    private void setNumberOfTickets(int numberOfTickets) {
+        if (numberOfTickets <= 0) {
+            throw new IllegalArgumentException("Number of tickets must be above 0!");
+        }
+        this.numberOfTickets = numberOfTickets;
     }
 }
